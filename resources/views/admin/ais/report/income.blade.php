f4c4a1a237738ab12333e11451a70db145d01416
Nazia <nnazia688@gmail.com>
2020-03-16 12:55:14 +0600

    income statement

--- resources/views/admin/ais/report/income.blade.php
+++ resources/views/admin/ais/report/income.blade.php
@@ -1,10 +1,21 @@
 @extends('admin.master')
 
+<style>
+    ul {
+        padding-inline-start: 25px;
+    }
+    hr {
+        margin-top: 0.2rem !important;
+        margin-bottom: 0.2rem !important;
+        border: 0 !important;
+        border-top: 1px solid rgba(45, 130, 200, 0.3) !important;
+    }
+</style>
 
 @section('content')
 
     <samp>
-        <div class="col-md-7">
+        <div class="col-md-11">
             <div class="card text-left">
                 <div class="card-header">
                     Income Statement
@@ -21,119 +32,131 @@
                     </form>
 
                 </div>
-                <div class="card-footer">
-
+                <div class="card-body">
                     <div class="tree">
                         <ul>
-                            @foreach( $heads as $head)
-                                <li>
-                            <span>
-                                {!! count( $head->child )>0 ? '<i class="fa fa-minus-square"></i> '.$head->name : $head->name !!}
-                                <b class="float-right">
-                                    @php( $balance = $all_bl->whereIn('thead_id', $head->theads->pluck('id')))
-                                    @php( $bl[$head->id] = $head->id !=4 ? $balance->sum('credit') - $balance->sum('debit') : $balance->sum('debit') - $balance->sum('credit'))
-                                    {!! $bl[$head->id] !!}
-                                </b>
-                            </span>
-
-                                    <ul>
-                                        @foreach( $head->child as $child_i )
-                                            <li>
+                            <div class="row">
+                                @foreach( $heads as $head)
+                                    <div class="col-md-6" style="border-left: 1px solid rgba(45, 130, 200, 0.5); border-right: 1px solid rgba(45, 130, 200, 0.5);">
+                                        <li>
                                         <span>
-                                                {!! count( $child_i->child )>0 ? '<i class="fa fa-minus-square"></i> '.$child_i->name : $child_i->name !!}
+                                            <b style="color: #007bff;"> {!! '<i class="fa fa-minus-square"></i> '.$head->name !!} </b>
+                                            <b class="float-right" style="color: #007bff;">
+                                                @php( $balance = $all_bl->whereIn('thead_id', $head->theads->pluck('id')))
+                                                @php( $bl[$head->id] = $head->id !=4 ? $balance->sum('credit') - $balance->sum('debit') : $balance->sum('debit') - $balance->sum('credit'))
+{{--                                                {!! $bl[$head->id] !!}--}}
+                                            </b>
                                         </span>
-
-                                                <ul>
-                                                    @foreach( $child_i->child as $child_ii )
-                                                        <li>
+                                            <hr>
+                                            <ul>
+                                                @foreach( $head->child as $child_i )
+                                                    <li>
+                                        <span>
+                                                {!! '<i class="fa fa-minus-square"></i> '.$child_i->name !!}
+                                        </span>
+                                                        <hr>
+                                                        <ul>
+                                                            @foreach( $child_i->child as $child_ii )
+                                                                <li>
                                                             <span>
-                                                                {!! count( $child_ii->child )>0 ? '<i class="fa fa-minus-square"></i> '.$child_ii->name : $child_ii->name !!}
+                                                                {!! '<i class="fa fa-minus-square"></i> '.$child_ii->name !!}
                                                             </span>
-
-                                                            <ul>
-                                                                @foreach( $child_ii->child as $child_iii )
-                                                                    <li>
+                                                                    <hr>
+                                                                    <ul>
+                                                                        @foreach( $child_ii->child as $child_iii )
+                                                                            <li>
                                                                 <span>
-                                                                    {!! count( $child_iii->child )>0 ? '<i class="fa fa-minus-square"></i> '.$child_iii->name : $child_iii->name !!}
+                                                                    {!! '<i class="fa fa-minus-square"></i> '.$child_iii->name !!}
                                                                 </span>
+                                                                                <hr>
+                                                                                <ul>
+                                                                                    @foreach( $child_iii->child as $child_iv )
+                                                                                        <li>
+                                                                                            <span>{{ $child_iv->name }}</span>
+                                                                                            <hr>
+                                                                                        </li>
+                                                                                    @endforeach
 
-                                                                        <ul>
-                                                                            @foreach( $child_iii->child as $child_iv )
-                                                                                <li>
-                                                                                    <span>{{ $child_iv->name }}</span>
-                                                                                </li>
-                                                                            @endforeach
-
-                                                                            @foreach( $child_iii->transaction as $transaction )
-                                                                                @if( $all_bl->where('thead_id', $transaction->id)->sum('credit')>0 || $all_bl->where('thead_id', $transaction->id)->sum('debit')>0 )
-                                                                                    <li>
+                                                                                    @foreach( $child_iii->transaction as $transaction )
+                                                                                        @if( $all_bl->where('thead_id', $transaction->id)->sum('credit')>0 || $all_bl->where('thead_id', $transaction->id)->sum('debit')>0 )
+                                                                                            <li>
                                                                                         <span>
                                                                                             <i class="fa fa-tag" aria-hidden="true"></i>
                                                                                             {{ $transaction->name }} [ {{ $transaction->code }} ]  <b class="float-right">{{ $all_bl->where('thead_id', $transaction->id)->sum('credit') - $all_bl->where('thead_id', $transaction->id)->sum('debit') }}</b>
                                                                                         </span>
-                                                                                    </li>
-                                                                                @endif
-                                                                            @endforeach
-                                                                        </ul>
-                                                                    </li>
-                                                                @endforeach
+                                                                                                <hr>
+                                                                                            </li>
+                                                                                        @endif
+                                                                                    @endforeach
+                                                                                </ul>
+                                                                            </li>
+                                                                        @endforeach
 
-                                                                @foreach( $child_ii->transaction as $transaction )
-                                                                    @if( $all_bl->where('thead_id', $transaction->id)->sum('credit')>0 || $all_bl->where('thead_id', $transaction->id)->sum('debit')>0)
-                                                                        <li>
+                                                                        @foreach( $child_ii->transaction as $transaction )
+                                                                            @if( $all_bl->where('thead_id', $transaction->id)->sum('credit')>0 || $all_bl->where('thead_id', $transaction->id)->sum('debit')>0)
+                                                                                <li>
                                                                             <span>
                                                                                 <i class="fa fa-tag" aria-hidden="true"></i>
                                                                                 {{ $transaction->name }} [ {{ $transaction->code }} ]  <b class="float-right">{{ $all_bl->where('thead_id', $transaction->id)->sum('credit') - $all_bl->where('thead_id', $transaction->id)->sum('debit') }}</b>
                                                                             </span>
-                                                                        </li>
-                                                                    @endif
-                                                                @endforeach
-                                                            </ul>
-                                                        </li>
-                                                    @endforeach
+                                                                                    <hr>
+                                                                                </li>
+                                                                            @endif
+                                                                        @endforeach
+                                                                    </ul>
+                                                                </li>
+                                                            @endforeach
 
-                                                    @foreach( $child_i->transaction as $transaction )
-                                                        @if( $all_bl->where('thead_id', $transaction->id)->sum('credit')>0 || $all_bl->where('thead_id', $transaction->id)->sum('debit')>0)
-                                                            <li>
+                                                            @foreach( $child_i->transaction as $transaction )
+                                                                @if( $all_bl->where('thead_id', $transaction->id)->sum('credit')>0 || $all_bl->where('thead_id', $transaction->id)->sum('debit')>0)
+                                                                    <li>
                                                         <span>
                                                             <i class="fa fa-tag" aria-hidden="true"></i>
                                                             {{ $transaction->name }} [ {{ $transaction->code }} ] <b class="float-right">{{ $all_bl->where('thead_id', $transaction->id)->sum('credit') - $all_bl->where('thead_id', $transaction->id)->sum('debit') }}</b>
                                                         </span>
-                                                            </li>
-                                                        @endif
-                                                    @endforeach
-                                                </ul>
+                                                                        <hr>
+                                                                    </li>
+                                                                @endif
+                                                            @endforeach
+                                                        </ul>
 
-                                            </li>
-                                        @endforeach
-                                        @foreach( $head->transaction as $transaction )
-                                            <li>
+                                                    </li>
+                                                @endforeach
+                                                @foreach( $head->transaction as $transaction )
+                                                    <li>
                                             <span>
                                                 <i class="fa fa-tag" aria-hidden="true"></i>
                                                 {{ $transaction->name }} [ {{ $transaction->code }} ]
                                                 <b>Amount</b>
                                             </span>
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                </li>
-                                <br><br><br>
-
-                            @endforeach
-
+                                                        <hr>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                        </li>
+                                    </div>
+                                @endforeach
+                            </div>
                         </ul>
                     </div>
                 </div>
+                <div class="card-footer">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h4><b class="pull-left" style="color: rgba(46, 204, 113, 0.7);">Total Income</b> <b class="float-right">{{ $bl[3] }}</b></h4>
+                        </div>
+                        <div class="col-md-6">
+                            <h4><b class="pull-left" style="color: rgba(217, 30, 24, 0.7);">Total Expense</b> <b class="float-right">{{ $bl[4] }}</b></h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <h4><b class="pull-left" style="color: #007bff;">Revenue</b> <b class="float-right">{{ $bl[3] -$bl[4] }}</b></h4>
+                </div>
             </div>
-            <h4><b class="pull-left">Revenue</b> <b class="float-right">{{ $bl[3] -$bl[4] }}</b></h4>
+
         </div>
     </samp>
-
-    <br><br>
-    <br><br>
-
-
-
 
 
 @endsection
@@ -142,10 +165,6 @@
 @section('style')
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
 @endsection
-
-
-
-
 
 
 @section('script')
